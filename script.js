96808900e255f53a33b1ab77734238e537f09ac1
Kiri <kirilynne@gmail.com>
2021-09-29 12:59:05 -0400

    added psuedocode to script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var $hourBlocks = $(".hour-blocks");
 var $workDayScheduler = $(".work-day-scheduler");
 var currentDate = moment().format("dddd MMMM Do YYYY");
 var currentHour = moment().format("H");
+
+//open scheduler when nothing is inputted yet--when document loads
+
+//display currentDate
+
+//input text to text column--save to local storage on click
+
+//add different colors to show past, present and future hours
+
+//
