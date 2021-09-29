7fe2f722762811d23dfce5ae87a132f127468119
Kiri <kirilynne@gmail.com>
2021-09-29 17:41:44 -0400

    set current day time stamp in script.js

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 var $currentDay = $("#currentDay");
 var $hourBlocks = $(".hour-blocks");
 var $workDayScheduler = $(".work-day-scheduler");
-var currentDate = moment().format("dddd MMMM Do YYYY");
+
 var currentHour = moment().format("H");
 
 //open scheduler when nothing is inputted yet--when document loads
 
 //display currentDate
 
+var currentDate = moment().format("dddd MMMM Do YYYY");
+$("#time-stamp").text(currentDate);
+
+
 //input text to text column--save to local storage on click
 
 //add different colors to show past, present and future hours
 
-//
+var dataHour = ".data-hour"
+
+if (dataHour < currentHour) {
+
+    //style it green
+
+} else if
+
+    (dataHour > currentHour) {
+
+    //style it gray
+} else {
+
+    //style it red;
+
+}
+
