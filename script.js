33e3eae583032032520a75d5581de1f5aad8953f
Kiri <kirilynne@gmail.com>
2021-09-30 18:14:35 -0400

    retrieve content with getItem in script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 var $currentDay = $("#currentDay");
-var $hourBlocks = $(".hour-blocks");
-var $workDayScheduler = $(".work-day-scheduler");
 var currentDate = moment().format("dddd MMMM Do YYYY");
 var currentHour = moment().format("H");
 var hourNineEl = $('.data-hour-9');
@@ -184,78 +182,26 @@ if (17 < currentHour) {
 
 }
 
-//var saveButton = document.querySelector(".save-button");
+//save to local storage when user clicks save button
 
 $(".save-button").on("click", function () {
     var buttonClick = $(this).attr("id");
     var content = $(this).siblings(".text-area").val();
 
-    console.log(buttonClick);
-    console.log(content);
-
     localStorage.setItem(buttonClick, content);
+    localStorage.getItem(content);
 })
 
-localStorage.getItem(content);
+//retrieve saved input when page is refreshed
 
-//localStorage.getItem(" ", content);
-//input text to text column--save to local storage on click
-//inputted in textarea row -- do i add a new id for each row?
+$("#text-9").val(localStorage.getItem('save-9'));
+$("#text-10").val(localStorage.getItem('save-10'));
+$("#text-11").val(localStorage.getItem('save-11'));
+$("#text-12").val(localStorage.getItem('save-12'));
+$("#text-13").val(localStorage.getItem('save-13'));
+$("#text-14").val(localStorage.getItem('save-14'));
+$("#text-15").val(localStorage.getItem('save-15'));
+$("#text-16").val(localStorage.getItem('save-16'));
+$("#text-17").val(localStorage.getItem('save-17'));
 
-/*var hourNineText = document.querySelector("#text-9");
-var hourNineSave = document.querySelector("#save-9");
-var savedNine = document.querySelector("#text-9");
-var hourNineTextEl = $("#text-9");
 
-hourNineSave.addEventListener("click", saveNine);
-
-savedNine.textContent = localStorage.getItem("content");
-hourNineText.value = localStorage.getItem("content");
-
-function saveNine() {
-    localStorage.setItem("content", hourNineText.value);
-
-    hourNineTextEl.text(hourNineText.value);
-}
-
-//works by itself but when i use a similar strategy below--everything duplicates???
-
-var hourTenText = document.querySelector("#text-10");
-var hourTenSave = document.querySelector("#save-10");
-var savedTen = document.querySelector("#text-10");
-var hourTenTextEl = $("#text-10");
-
-hourTenSave.addEventListener("click", saveTen);
-
-savedTen.textContent = localStorage.getItem("content");
-hourTenText.value = localStorage.getItem("content");
-
-function saveTen() {
-    localStorage.setItem("content", hourTenText.value);
-
-    hourTenTextEl.text(hourTenText.value);
-}*/
-
-/*
-var hourElevenText = document.querySelector("#text-11");
-var hourElevenSave = document.querySelector("#save-11");
-
-var hourTwelveText = document.querySelector("#text-12");
-var hourTwelveSave = document.querySelector("#save-12");
-
-var hourThirteenText = document.querySelector("#text-13");
-var hourThirteenSave = document.querySelector("#save-13");
-
-var hourFourteenText = document.querySelector("#text-14");
-var hourFourteenSave = document.querySelector("#save-14");
-
-var hourFifteenText = document.querySelector("#text-15");
-var hourFifteenSave = document.querySelector("#save-15");
-
-var hourSixteenText = document.querySelector("#text-16");
-var hourSixteenSave = document.querySelector("#save-16");
-
-var hourSeventeenText = document.querySelector("#text-17");
-var hourSeventeenSave = document.querySelector("#save-17");
-
-*/
